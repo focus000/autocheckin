9b22c5663afb5a0cd7c905ec8787afe610704e06
yunfangli <hanszimmerme@gmail.com>
2020-05-11 12:17:51 +0800

    add user and reformat

--- main.py
+++ main.py
@@ -12,23 +12,27 @@ knames = [
     "cgdd", "gldd", "jzyy", "bllb", "sfjctr", "jcrysm", "xgjcjlsj", "xgjcjldd",
     "xgjcjlsm", "zcwd", "zwwd", "wswd", "sbr", "sjd"
 ]
-persons_id = [220170918811, 220170918911, 220170918711, 220170918721, 220170918421, 220170919001, 220170918971, 220170919031, 220170918551]
+persons_id = [
+    220170918811, 220170918911, 220170918711, 220170918721, 220170918421,
+    220170919001, 220170918971, 220170919031, 220170918551, 220170918891
+]
 
 sched = BlockingScheduler(timezone=timez)
+
 
 @sched.scheduled_job('cron', id='my_job', hour='6,10,18', minute=40)
 def job():
     for single_id in persons_id:
         p = requests.post("http://202.201.13.180:9037/encryption/getMD5",
-                        data={'cardId': single_id})
+                          data={'cardId': single_id})
         if p.json()['code'] != 1:
             logging.warning(str(single_id) + "getMD5 failed")
             continue
         r = requests.post("http://202.201.13.180:9037/grtbMrsb/getInfo",
-                        data={
-                            'cardId': single_id,
-                            'md5': p.json()['data']
-                        })
+                          data={
+                              'cardId': single_id,
+                              'md5': p.json()['data']
+                          })
         data = r.json()
         if data['code'] != 1:
             logging.warning(str(single_id) + "getInfo failed")
@@ -41,9 +45,10 @@ def job():
         data['zcwd'] = random.randrange(360, 369) / 10
         data['zwwd'] = random.randrange(360, 369) / 10
         data['wswd'] = random.randrange(360, 369) / 10
-        ndata = {key : value for key, value in data.items() if key in knames}
+        ndata = {key: value for key, value in data.items() if key in knames}
         logging.info(ndata)
-        f = requests.post("http://202.201.13.180:9037/grtbMrsb/submit", data=ndata)
+        f = requests.post("http://202.201.13.180:9037/grtbMrsb/submit",
+                          data=ndata)
         # print(f.json())
         if f.json()['code'] != 1:
             logging.warning(f.json())
@@ -51,5 +56,6 @@ def job():
             continue
         else:
             logging.info(f.json())
+
 
 sched.start()
